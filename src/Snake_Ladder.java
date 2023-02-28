a80ece761052300e89b2d7863956040a3739cb28
debabrata-97 <debabrataszr@gmail.com>
2023-02-28 13:46:29 +0530

    Play the game with Two Players

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,28 +1,47 @@
 public class Snake_Ladder {
     static int winposition = 100;
-
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator Program");
-        int PlayerPos = 0;
-        System.out.println("Player is at start Position " + PlayerPos);
+        int Player1Pos = 0;
+        int Player2Pos = 0;
+        System.out.println("Player is at start Position " + Player1Pos);
         int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
         System.out.println("Player rolls the die and gets " + randomCheck);
+
+        //player1
         int Option = (int) Math.floor(Math.random() * 10) % 3;
         switch (Option) {
             case 0:
                 System.out.println("Ladder \nPlayer moves forward by " + randomCheck);
-                PlayerPos += randomCheck;
+                Player1Pos += randomCheck;
                 break;
             case 1:
                 System.out.println("Snake \nPlayer moves behind by " + randomCheck);
-                PlayerPos -= randomCheck;
+                Player1Pos -= randomCheck;
                 break;
             default:
                 System.out.println("NO PLAY \nPlayer remains in the same position " + randomCheck);
-                PlayerPos += 0;
+                Player1Pos += 0;
                 break;
         }
-        if (winposition > PlayerPos) {
+
+        //player2
+        int Option1 = (int) Math.floor(Math.random() * 10) % 3;
+        switch (Option1) {
+            case 0:
+                System.out.println("Ladder \nPlayer moves forward by " + randomCheck);
+                Player2Pos += randomCheck;
+                break;
+            case 1:
+                System.out.println("Snake \nPlayer moves behind by " + randomCheck);
+                Player2Pos -= randomCheck;
+                break;
+            default:
+                System.out.println("NO PLAY \nPlayer remains in the same position " + randomCheck);
+                Player2Pos += 0;
+                break;
+        }
+        if (winposition > Player1Pos) {
             winposition = 0;
         }
         if (winposition > 100) {
@@ -32,8 +51,9 @@ public class Snake_Ladder {
         randomCheck++;
 
         System.out.println("Total Number Of Times Die Rolled Are : " + randomCheck);
+
+        if (Player2Pos == 100) {
+            System.out.println("winner player 1 !! " + Player1Pos + "/100");
+        } else System.out.println("winner player 2 !! " + Player2Pos + "/100");
     }
 }
-
-
-
