75a7cafacf89295d216e37afc404a21d0ef6ecad
debabrata-97 <debabrataszr@gmail.com>
2023-02-28 12:25:43 +0530

    Single player at start position

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,5 +1,7 @@
 public class Snake_Ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator Program" );
+        int PlayerPos = 0;
+        System.out.println("Player is at start Position " + PlayerPos);
     }
 }
