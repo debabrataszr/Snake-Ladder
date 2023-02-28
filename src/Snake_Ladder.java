175dfd55c9df400a13560988995303fee38b3d5b
debabrata-97 <debabrataszr@gmail.com>
2023-02-28 12:31:45 +0530

    Player checking for an option

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -5,5 +5,20 @@ public class Snake_Ladder {
         System.out.println("Player is at start Position " + PlayerPos);
         int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
         System.out.println("Player rolls the die and gets " + randomCheck);
+        int Option = (int) Math.floor(Math.random() * 10) % 3;
+        switch (Option) {
+            case 0:
+                System.out.println("Ladder \nPlayer moves forward by " + randomCheck);
+                PlayerPos += randomCheck;
+                break;
+            case 1:
+                System.out.println("Snake \nPlayer moves behind by " + randomCheck);
+                PlayerPos -= randomCheck;
+                break;
+            default:
+                System.out.println("NO PLAY \nPlayer remains in the same position " + randomCheck);
+                PlayerPos += 0;
+                break;
+        }
     }
 }
