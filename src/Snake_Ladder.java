bfec8cfdaeb17cecf4bf98d5b55c8035e4f23d02
debabrata-97 <debabrataszr@gmail.com>
2023-02-28 12:28:53 +0530

    Player rolls the die

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -3,5 +3,7 @@ public class Snake_Ladder {
         System.out.println("Welcome to Snake and Ladder Simulator Program" );
         int PlayerPos = 0;
         System.out.println("Player is at start Position " + PlayerPos);
+        int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("Player rolls the die and gets " + randomCheck);
     }
 }
